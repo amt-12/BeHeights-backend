e5bb99fd64867e371289e78505a9bb401e4b7809
amt-12 <2020010004188@gndu.ac.in>
2024-12-28 15:21:21 +0530

    Add route to retrieve a single user by ID

--- routes/api/User.route.js
+++ routes/api/User.route.js
@@ -12,5 +12,6 @@ router.get("/me", validateAccessToken, currentUser);
 router.get("/admin/me", validateAccessToken, adminCurrentUser);
 router.put("/updatepassword/:id", validateAccessToken, updatePassword);
 router.get("/", getAllUsers);
+
 router.put("/:id", validateAccessToken, getSingleUser);
 module.exports = router;
