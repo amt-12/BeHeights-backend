cc5cd47e51aee96581949d435fe24f201234ef79
amt-12 <2020010004188@gndu.ac.in>
2024-12-31 01:30:49 +0530

    Update MongoDB connection string in index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const mongoose = require("mongoose");
 const database = process.env.DB_CONNECT;
 
 mongoose
-  .connect(database, {
+  .connect("mongodb+srv://amrit0207232:Cdjj2NB7bRb16Y78@beheights.facpumu.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
